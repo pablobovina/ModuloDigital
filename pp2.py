156691c56a12f85bf083a2e19ee1d6fac352c1b7
pablobovina <pablobovina@gmail.com>
2018-07-03 10:18:50 -0300

    limpio codigo de logs innecesarios

--- pp2.py
+++ pp2.py
@@ -9,11 +9,9 @@ from Usb import Usb
 from secuence import Secuence
 from time import sleep
 
+
 class Pp2(object):
     """clase para controlar el pp2"""
-
-    # codigos de estados del pp2
-    op_codes = {"execute": 1, "on": 2, "reset": 3, "off": 4, "upload": 5, "trigger": 6}
 
     def __init__(self, mod_id=1, delay=5, amount=200):
         """
@@ -21,9 +19,6 @@ class Pp2(object):
             delay: en milisegundos, intervalo de tiempo entre comandos
         """
         self.mod_id = mod_id
-        self.last_request = None
-        self.requested_operations = []
-        self.status = None
         self.cmd = []
         self.delay = delay
         self.secuence = None
@@ -46,37 +41,18 @@ class Pp2(object):
             self.cmd.append((['T', chr(0x52), chr(0x00)], 4))
 
         self._execute()
-
-        print "pp2 upload program"
-        self.requested_operations.append(self.op_codes['upload'])
-        self.last_request = self.op_codes['upload']
-        self.status = self.op_codes['upload']
-
         return True
 
     def trigger_program(self):
-
         # instruccion disparo de secuencia de pulsos
         self.cmd.append((['M', chr(0x50), chr(0x00)], 4))
         self.cmd.append((['D', chr(0x50), chr(0x00), chr(0x80)], 4))
         self._execute()
-
-        print "pp2 trigger program"
-        self.requested_operations.append(self.op_codes['trigger'])
-        self.last_request = self.op_codes['trigger']
-        self.status = self.op_codes['trigger']
-
         return True
 
     def _execute(self):
         """ejecutar pila de instucciones del pp2"""
-
         data = self.interfaz.execute(self.delay, self.cmd)
-
-        #print "pp2 execute"
-        self.requested_operations.append(self.op_codes['execute'])
-        self.last_request = self.op_codes['execute']
-        self.status = self.op_codes['execute']
         self.cmd = []
         return data
 
@@ -87,14 +63,8 @@ class Pp2(object):
         op = ['E', chr(0x52), chr(0x00)]
         while intentos < self.amount:
             response = self.interfaz.request(op, 4)
-            print response.value
-            print "pp2 wait end run"
             if ord(response.value[0]) & 0x01:
-                intentos = self.amount
                 flag = True
-                print "caracter esperado para cortar"
-                print response.value
-                print ord(response.value[0])
                 break
             else:
                 intentos += 1
