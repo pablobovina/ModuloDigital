6d8ba226700d2aa8e7a6dac94ffab56f2c410d1b
pablobovina <pablobovina@gmail.com>
2018-07-10 20:27:23 -0300

    agrego chequeos de parametros de conguracion del AD

--- ad.py
+++ ad.py
@@ -7,36 +7,73 @@ Modulo de control del AD
 
 from Usb import Usb
 from time import sleep
+import logging
 
 
 class Ad(object):
     """clase para controlar el AD"""
-    channels = {0x08: "canalB", 0x0A: "canalA", 0x09: "canalAB"}
-    bloq_base = {16: 0x00, 32: 0x10, 64: 0x20, 128: 0x30, 256: 0x40, 512: 0x50, 1024: 0x60, 2048: 0x70}
+    bloq_base = {1: 0x00, 2: 0x10, 4: 0x20, 8: 0x30, 16: 0x40, 32: 0x50, 64: 0x60, 128: 0x70}
 
-    def __init__(self, mod_id=1, delay=5, amount=200,  bloqnum=16, inter_ts=1000000, channel=2):
+    def __init__(self, mod_id=1, delay=0, amount=200, bloqnum=16, inter_ts=1000000):
         """
             id: entero, identificador de la instancia de dds2
             delay: en milisegundos, intervalo de tiempo entre comandos
         """
+        errors = []
+        self._check_mod_id(mod_id, errors)
+        self._check_delay(delay, errors)
+        self._check_amount(amount, errors)
+        self._check_block_num(bloqnum, errors)
+        self._check_inter_ts(inter_ts, errors)
+
+        if errors:
+            for error in errors:
+                logging.error(error)
+            raise Exception("error create instance AD " + ";".join(errors))
+
         self.mod_id = mod_id
-        self.last_request = None
-        self.requested_operations = []
         self.cmd = []
-        self.history = []
-        self.status = None
         self.delay = delay
-        self.channel = channel
         self.channel_a = None
         self.channel_b = None
         self.channel_ab = None
         self.data_a = None
         self.data_b = None
-        self.buffer_len = bloqnum * 64
+        self.buffer_len = bloqnum * 1024
         self.bloqnum = self.bloq_base.get(bloqnum, 0x00)
         self.inter_ts = inter_ts
         self.amount = amount
         self.interfaz = Usb()
+
+    @staticmethod
+    def _check_mod_id(mod_id, errors=None):
+        if mod_id < 0 or not isinstance(mod_id, int):
+            errors.append("mod_id debe ser un numero entero mayor a 0")
+        return
+
+    @staticmethod
+    def _check_delay(delay, errors=None):
+        if not (0 <= delay <= 1000 and isinstance(delay, int)):
+            errors.append("delay debe ser un entero en [0,1000]")
+        return
+
+    @staticmethod
+    def _check_amount(amount, errors=None):
+        if not (0 <= amount <= 500 and isinstance(amount, int)):
+            errors.append("amount debe ser un entero en [0,500]")
+        return
+
+    @staticmethod
+    def _check_block_num(bloqnum, errors=None):
+        if bloqnum not in Ad.bloq_base.keys():
+            errors.append("bloqnum debe estar en {}".format(Ad.bloq_base.keys()))
+        return
+
+    @staticmethod
+    def _check_inter_ts(inter_ts, errors=None):
+        if not (100 <= inter_ts <= 254000 and isinstance(inter_ts, int)):
+            errors.append("inter_ts debe enteno en nanosegundos [100, 254000]")
+        return
 
     def configure(self):
         self._reset10()
@@ -94,11 +131,10 @@ class Ad(object):
         return
 
     def _set_time(self):
-        if self.inter_ts * .0000001 != 0:
-            muestras = self.inter_ts / 100.
-            delta_t = 255 - int(muestras)
-            data = ['t', chr(0x0c), chr(delta_t)]
-            self.cmd.append((data, 4))
+        muestras = self.inter_ts / 100.
+        delta_t = 255 - int(muestras)
+        data = ['t', chr(0x0c), chr(delta_t)]
+        self.cmd.append((data, 4))
         return
 
     def _leer(self, channel_dir):
@@ -114,17 +150,17 @@ class Ad(object):
 
     def _combinar(self, l1, l2, l3):
 
-        l = max(len(l1), len(l2), len(l3))
+        max_len = max(len(l1), len(l2), len(l3))
         l1 = self._completar(l1)
         l2 = self._completar(l2)
         l3 = self._completar(l3)
-        print l
+        print max_len
         ca = []
         cb = []
 
         # usb.CanalB[i] = ((usb.Canal3[i]) & 0x0f) + (usb.Canal1[i] * 16);
         # usb.CanalA[i] = ((usb.Canal3[i] / 16) & 0x0f) + ((usb.Canal2[i]) * 16);
-        for i in range(l):
+        for i in range(max_len):
                 ca.append((i, (ord(l3[i]) & 0x0F) + ord(l1[i]) * 16))
                 cb.append((i, (ord(l3[i])/16) & 0x0F + ord(l2[i]) * 16))
         return ca, cb
