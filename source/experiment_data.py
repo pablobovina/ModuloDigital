5881a78a0161740bad78e1f6f9080c1ec4291ca0
Pablo Bovina <pablo.bovina@incluit.com>
2018-08-01 19:40:35 -0300

    manejo de errores para a_times y cpoints

--- source/experiment_data.py
+++ source/experiment_data.py
@@ -1,8 +1,19 @@
+import logging
 
 
 class ExperimentData:
 
     def __init__(self, cpoints, repeat):
+
+        errors = []
+        self._check_cpoints(cpoints, errors)
+        self._check_repeat(repeat, errors)
+
+        if errors:
+            for error in errors:
+                logging.error(error)
+            raise Exception("error create experiment data " + ";".join(errors))
+
         self.repeat = repeat
         self.cpoints = cpoints
 
@@ -26,3 +37,15 @@ class ExperimentData:
         phase_list = cpoint["phase"]
         cpoint["phase"] = phase_list[1:] + phase_list[:1]
         return cpoint
+
+    @staticmethod
+    def _check_repeat(repeat, errors=None):
+        if not (repeat > 0 and isinstance(repeat, int)):
+            errors.append("repeat debe ser un numero entero mayor a 0")
+        return
+
+    @staticmethod
+    def _check_cpoints(cpoints, errors=None):
+        if not (cpoints and isinstance(cpoints, list)):
+            errors.append("cpoints debe ser un una lista no vacia de instrucciones")
+        return
